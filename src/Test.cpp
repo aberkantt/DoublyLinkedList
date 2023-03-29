2ecf6ad2d27f65a6a95ec1a33b006b36d0eea215
aberkantt <berkant.ontemel@ogr.sakarya.edu.tr>
2023-03-29 17:51:33 +0300

    DoublyLinkedList

---
+++ src/Test.cpp
@@ -0,0 +1,15 @@
+#pragam GCC diagnostic ignored "-Wdeprecated"
+#include <cstdlib>
+#include "DoublyLinkedList.hpp"
+
+int main(){
+	DoublyLinkedList<string> *isimler = new DoublyLinkedList<string>();
+	int sec=0;
+	string isim;
+	int konum;
+	
+	do{
+		system("CLS");
+		cout<<"1.Isim Ekle"<<endl;
+	}while()
+}
